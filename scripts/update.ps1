cc5d28d1f278a5a3ba99220ac946cb8ba43800ba
Tim Pilius <tpilius@gmail.com>
2023-02-11 17:54:29 -0500

    Adding update scripts

---
+++ scripts/update.ps1
@@ -0,0 +1,26 @@
+Set-Location $PSScriptRoot
+
+# Getting latest version tag
+Write-Host -ForegroundColor Yellow "Checking for latest version"
+$versionApi = "https://api.github.com/repos/tpill90/epic-lancache-prefill/releases"
+$versions = Invoke-RestMethod -Uri $versionApi
+
+# Finding latest asset
+$windowsAsset = $versions[0].assets | Where-Object { $_.name.Contains("win-x64")}
+Write-Host "Found latest version : " -NoNewline
+Write-Host -ForegroundColor Cyan $windowsAsset.name
+
+# Downloading
+Write-Host -ForegroundColor Yellow "Downloading..."
+Invoke-WebRequest $windowsAsset.browser_download_url -OutFile $windowsAsset.name
+
+# Unzipping
+Write-Host -ForegroundColor Yellow "Unzipping..."
+Expand-Archive -Force $windowsAsset.name -DestinationPath .
+Copy-Item "$($windowsAsset.name.Replace('.zip', ''))\EpicPrefill.exe"
+
+# Cleanup 
+Remove-Item $windowsAsset.name
+Remove-Item -Force -Recurse "$($windowsAsset.name.Replace('.zip', ''))"
+
+Write-Host -ForegroundColor Cyan "Complete !"
