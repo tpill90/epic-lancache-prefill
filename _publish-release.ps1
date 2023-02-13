6ffeac500297f526bdb867a80f73937c8fbdd587
Tim Pilius <tpilius@gmail.com>
2023-02-13 12:03:00 -0500

    Adding publish script

---
+++ _publish-release.ps1
@@ -0,0 +1,23 @@
+Push-Location $PSScriptRoot
+clear
+
+# Getting current version
+[xml]$parsedCsproj = Get-Content .\EpicPrefill\EpicPrefill.csproj
+$versionPrefix = $parsedCsproj.Project.PropertyGroup.VersionPrefix
+$currentVersion = "$versionPrefix".Trim();
+Write-Color "Current version: ", $currentVersion -Color White, Yellow
+
+# Getting new version to use
+$newVersion = Read-Host "Enter new version, with no leading 'v'.  Ex. '1.2.3'"
+
+# Updating csproj version
+$currentContent = Get-Content -Path .\EpicPrefill\EpicPrefill.csproj -Raw
+$currentContent = $currentContent.Replace('<VersionPrefix>' + $currentVersion, '<VersionPrefix>' + $newVersion)
+Set-Content -Value $currentContent -Path .\EpicPrefill\EpicPrefill.csproj -NoNewline
+
+# Committing + tag.  Pushing the tag is what creates the release.
+git commit -a -m "v$newVersion"
+git tag "v$newVersion"
+git push --tags
+
+Pop-Location
---
+++ _startMkdocs.ps1
@@ -0,0 +1,4 @@
+pip install mkdocs-awesome-pages-plugin
+cd docs
+start http://127.0.0.1:8000
+python3 -m mkdocs serve
